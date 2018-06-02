d0237b43ce907592e9433320d348a543b22d80cc
FLooD <floodwm@yandex.ru>
2018-06-02 16:46:31 +0300

    admin pannel

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,7 +1,78 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: olgaradzunas
- * Date: 02.06.2018
- * Time: 15:29
- */
+@extends('admin.layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-3">
+                <div class="jumbotron">
+                    <span class="label label-primary">Category
+                            <span class="badge badge-secondary">0</span>
+                        </span>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="jumbotron">
+                    <span class="label label-primary">Articles
+                            <span class="badge badge-secondary">0</span>
+                        </span>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="jumbotron">
+                    <span class="label label-primary">People
+                            <span class="badge badge-secondary">0</span>
+                        </span>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="jumbotron">
+                    <span class="label label-primary">To Day
+                            <span class="badge badge-secondary">0</span>
+                        </span>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-6 list-group">
+                <a href="#" class="btn btn-block btn btn-info">CREATE CATEGORY</a>
+                <p></p>
+                <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h5 class="mb-1">List group item heading</h5>
+                        <small class="text-muted">3 days ago</small>
+                    </div>
+                    <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+                    <small class="text-muted">Donec id elit non mi porta.</small>
+                </a>
+                <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h5 class="mb-1">List group item heading</h5>
+                        <small class="text-muted">3 days ago</small>
+                    </div>
+                    <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+                    <small class="text-muted">Donec id elit non mi porta.</small>
+                </a>
+            </div>
+            <div class="col-6 list-group">
+                <a href="#" class="btn btn-block btn btn-info">CREATE ARTICLE</a>
+                <p></p>
+                <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h5 class="mb-1">List group item heading</h5>
+                        <small class="text-muted">3 days ago</small>
+                    </div>
+                    <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+                    <small class="text-muted">Donec id elit non mi porta.</small>
+                </a>
+                <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h5 class="mb-1">List group item heading</h5>
+                        <small class="text-muted">3 days ago</small>
+                    </div>
+                    <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+                    <small class="text-muted">Donec id elit non mi porta.</small>
+                </a>
+            </div>
+        </div>
+    </div>
+@endsection()
